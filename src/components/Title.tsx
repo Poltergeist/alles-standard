1b0d88056b715e404038d61b929f7073c94f8982
Poltergeist <philipp@sp3c1.com>
2024-10-17 10:32:56 +0200

    feat(page): add logo and new date

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,9 +1,23 @@
 import { ReactNode } from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+import { Box, Center } from '@chakra-ui/react';
 
 type TitleProps = {
   children: ReactNode;
 };
 
 export default function Title({ children }: TitleProps) {
-  return <h1>{children}</h1>;
+  return (
+    <Box>
+      <Center>
+        <StaticImage
+          src="../images/logo.svg"
+          alt="Alles Standard"
+          width={520}
+          placeholder="blurred"
+          style={{ marginRight: '8px' }}
+        />
+      </Center>
+    </Box>
+  );
 }
